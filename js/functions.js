e529ae75a5c33dfce6eff8cf75c787d1b0ea783d
Александр Волков <AVolkov211@yandex.ru>
2026-01-21 11:39:37 +0300

    Пятое ДЗ

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,10 @@
-function checkingSringLength(string, number) {
+/* function checkingSringLength(string, number) {
   return (string.length <= number) ? 'Условие выполняется ТЕРН ОП' : 'Условие не выполняется, строка больше указанной длинны ТЕРН ОП';
 }
 
 checkingSringLength('Четыре', 6);
 
+// eslint-disable-next-line no-unused-vars
 function CheckPalindrome(line) {
   const result = line.replaceAll(' ', '');
   const register = result.toLowerCase();
@@ -22,4 +23,23 @@ function CheckPalindrome(line) {
   }
 }
 
-CheckPalindrome('Лёша на полке клопа нашёл');
+CheckPalindrome('Лёша на полке клопа нашёл');*/
+
+function timeCalculation(beginningWorkingDay, endWorkingDay, startMeeting, duration) {
+
+  function convertMinutes(timeStr) {
+    const [hours, minutes] = timeStr.split(':').map((part) => parseInt(part, 10) || 0);
+    return hours * 60 + minutes;
+  }
+
+  beginningWorkingDay = convertMinutes(beginningWorkingDay);
+  endWorkingDay = convertMinutes(endWorkingDay);
+  startMeeting = convertMinutes(startMeeting);
+
+  if (startMeeting + duration <= endWorkingDay) {
+    return true;
+  }
+  return false;
+}
+
+timeCalculation('09:00', '8:0', '12:00', 60);
